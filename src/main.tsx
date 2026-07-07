0436051227288c139f369ff3a58948c36bf23e7c
Ignacio Espeche <igna03espeche@gmail.com>
2026-07-07 17:41:38 -0300

    feat: implementar ScrollToTop automatico al cambiar de ruta

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 // src/main.tsx
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+// Componente auxiliar que escucha los cambios de página y te tira arriba de todo
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]); // Se ejecuta al instante cada vez que cambia la ruta de la web
+
+  return null;
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ScrollToTop />
     <App />
   </StrictMode>,
 )
