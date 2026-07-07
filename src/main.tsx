5f205e68d6f7334499910092db0942b661539cba
Ignacio Espeche <igna03espeche@gmail.com>
2026-07-07 17:46:19 -0300

    style: corregir renderizado y aplicar scroll to top nativo en html

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
 // src/main.tsx
-import { StrictMode, useEffect } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-// Componente auxiliar que escucha los cambios de página y te tira arriba de todo
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]); // Se ejecuta al instante cada vez que cambia la ruta de la web
-
-  return null;
-};
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ScrollToTop />
     <App />
   </StrictMode>,
 )
