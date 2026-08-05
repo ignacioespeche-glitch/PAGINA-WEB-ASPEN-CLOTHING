edbe66d92da3534871927daf5bd7a5537f48f5cc
Ignacio Espeche <igna03espeche@gmail.com>
2026-08-05 13:28:51 -0300

    Fix syntax y agregar Cloudflare Function Proxy para Tiendanube

---
+++ functions/api-tiendanube/[[path]].js
@@ -0,0 +1,29 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const targetPath = url.pathname.replace(/^\/api-tiendanube/, '');
+  const targetUrl = `https://api.tiendanube.com${targetPath}${url.search}`;
+
+  const modifiedHeaders = new Headers(context.request.headers);
+  modifiedHeaders.set('Host', 'api.tiendanube.com');
+
+  const response = await fetch(targetUrl, {
+    method: context.request.method,
+    headers: modifiedHeaders,
+    body: ['GET', 'HEAD'].includes(context.request.method) ? null : context.request.body
+  });
+
+  const responseHeaders = new Headers(response.headers);
+  responseHeaders.set('Access-Control-Allow-Origin', '*');
+  responseHeaders.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  responseHeaders.set('Access-Control-Allow-Headers', '*');
+
+  if (context.request.method === 'OPTIONS') {
+    return new Response(null, { status: 204, headers: responseHeaders });
+  }
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: responseHeaders
+  });
+}
